b4a6ce31b5401d8416cfab96def01ec2913b6493
attycsek007 <attila.vegh@ni.com>
2025-12-03 17:08:23 +0100

    Cleaned up the project linking

--- src/labview/auto-vision/Automotive Vision/1487 HIHO GSE/scripts/Image RT Scripts/Ser_Tap.py
+++
@@ -1,5 +0,0 @@
-board.WriteI2C(0x30, 0x01, 0x01) # Reset the Serializer
-time.sleep(0.25)
-#board.WriteI2C(0x30, 0x00, 0xF1) # Set I2C address to 0xF0 (0xF1 for reads) to prevent conflicts in tap mode
-board.WriteI2C(0x30, 0x32, 0x89) # Enable passthrough-all so that all I2C addresses that don't match 0xF1 are passed on
-board.WriteI2C(0x30, 0x09, 0x9E) # Disable remote writes to local register to prevent accident reconfiguration of the Serializer
--- src/labview/auto-vision/Automotive Vision/1487 HIHO GSE/scripts/Image Receiver Scripts/Des_Tap.py
+++
@@ -1,8 +0,0 @@
-board.WriteI2C(0x60, 0x01, 0x01) # Reset the Deserializer
-time.sleep(0.25)
-board.WriteI2C(0x60, 0x4C, 0x01) # Enable RX Port 0 configuration
-board.WriteI2C(0x60, 0x58, 0xDE) # Enable passthrough-all so that all I2C addresses that don't match this device's address are passed on
-board.WriteI2C(0x60, 0x33, 0x03) # Enable CSI-2 with continuous clock
-board.WriteI2C(0x60, 0x0C, 0x83) # Enable RX Ports
-board.WriteI2C(0x60, 0x20, 0x00) # Enable forwarding RX Port 0
-#board.WriteI2C(0x60, 0x00, 0xF3) # Set I2C Address to 0xF2 (0xF3 for reads) to prevent I2C address conflicts in tap mode
